6d9609f0388822ba5c9a2cf5970f3479d758fd86
AravindShounik <vangaaravindshounik@gmail.com>
2022-10-15 09:55:30 +0530

    Added Apartments solution

---
+++ Apartments.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> peeps(n);
+    vector<int> apps(m);
+    for (int i = 0; i < n; i++)
+        cin >> peeps[i];
+    for (int i = 0; i < m; i++)
+        cin >> apps[i];
+    sort(peeps.begin(), peeps.end());
+    sort(apps.begin(), apps.end());
+    int ans = 0;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (abs(peeps[i] - apps[j]) <= k)
+        {
+            ans++;
+            i++;
+            j++;
+        }
+        else if (peeps[i] < apps[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
